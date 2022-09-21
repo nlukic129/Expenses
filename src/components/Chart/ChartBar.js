994281c13f92c1ae8e724079f85895db6c746f43
nlukic129 <nlukic129@gmail.com>
2022-09-21 23:26:35 +0200

    Expense-react exercise

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./ChartBar.css";
 
 const ChartBar = (props) => {
-  let barFillHeight = "%";
+  let barFillHeight = 0;
   if (props.maxValue) {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
